d8797c1e71e0d4930318d0e70a663d96f0ecb47b
Meron G <merong@Merons-MacBook-Air.local>
2024-10-01 11:57:56 +0100

    finished day 2 exercises

--- src/main/java/com/corndel/supportbank/exercises/UserId.java
+++ src/main/java/com/corndel/supportbank/exercises/UserId.java
@@ -20,13 +20,25 @@ public class UserId {
     // If not, throw an IllegalArgumentException with the message
     // "The id must be an odd number of characters long"
 
+    if (this.id.length() % 2 == 0) {
+      throw new IllegalArgumentException("The id must be an odd number of characters long");
+    }
+
     // TODO: this.id must contain the letter 'a'
     // If not, throw an IllegalArgumentException with the message
     // "The id must contain the letter 'a'"
 
+    if (!this.id.contains("a")) {
+      throw new IllegalArgumentException("The id must contain the letter 'a'");
+    }
+
     // TODO: this.id must be all lowercase:
     // If not, throw an IllegalArgumentException with the message
     // "The id must be all lowercase"
+
+    if (!this.id.toLowerCase().equals(this.id)) {
+      throw new IllegalArgumentException("The id must be all lowercase");
+    }
 
     // If all validations pass, return true.
     return true;
